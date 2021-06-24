306a93b4eef8899af4a80bb351c10b585d071b13
Porika Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2021-06-24 20:41:32 +0530

    Assignment#6 1

---
+++ Assignment#2/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+F
+src/AssignmentTwo.java,0/f/0f3dceb18f618c11b629ad3c8a303bf74de64498
---
+++ Assignment#6/1/src/Vampire.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Vampire {
+    public static void main(String[] args) {
+        long t1 = System.currentTimeMillis();
+        String[] str1, str2;
+        int pro, sum = 0;
+        int count=100,c=0;
+        while(count!=0) {
+            c++;
+            for (int i = 10; i < 100; i++) {
+                for (int j = i; j < 100; j++) {
+                    pro = i * j;
+                    int lengthi = String.valueOf(i).length();
+                    int lengthj = String.valueOf(j).length();
+                    // Exclude numbers less than 1000 and greater than 999999
+                    if (lengthi != lengthj)
+                        continue;
+                    // Convert the product and multiplier to a string array
+                    str1 = String.valueOf(pro).split("");
+                    str2 = (String.valueOf(i) + String.valueOf(j)).split("");
+                    // Sort the string array
+                    Arrays.sort(str1);
+                    Arrays.sort(str2);
+                    // Compare two arrays
+                    if (Arrays.equals(str1, str2)) {
+                        sum++;
+                        System.out.println( i + "*" + j + "=" + pro);
+                    }
+                }
+            }
+            count--;
+        }
+    System.out.println("Vampire numbers count ---> "+c);
+    }
+
+}
